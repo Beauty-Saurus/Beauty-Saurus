9379984a8168736a3dea9fdd70c0db71ec4f11f1
kunlee <leekh716@naver.com>
2021-11-24 15:44:45 +0900

    :art: feat/feature-item link with settingDocu

---
+++ setting/setting-view/src/lib/bsmodules.js
@@ -0,0 +1,81 @@
+const customConfig = require("../../../../beauty.saurus.config.json");
+
+class NavItem {
+  constructor(type, docId, position, label) {
+    this.type = type;
+    this.docId = docId;
+    this.position = position;
+    this.label = label;
+  }
+
+  getNavItem() {
+    return {
+      type: this.type,
+      docId: this.docId,
+      position: this.position,
+      label: this.label,
+    };
+  }
+}
+
+exports.getNavItemsObj = function () {
+  const navItems = customConfig.navbar.items;
+  let navItemsRepo = [];
+
+  navItems.forEach((item) => {
+    const navObj = new NavItem(
+      item.type,
+      item.name + "/" + item.name,
+      item.position,
+      item.name
+    );
+    navItemsRepo.push(navObj);
+  });
+
+  console.log("navItemsRepo", navItemsRepo);
+  return navItemsRepo;
+};
+
+// function applycustomNavItems() {
+//   // back 에서 navItems[i].name 의 폴더를 만들어줘야 한다.
+//   const navItems = customConfig.navbar.items;
+//   console.log(typeof navItems);
+// }
+
+// applycustomNavItems();
+
+exports.applycustomCss = function () {
+  const root = document.querySelector(":root");
+  // console.log("customConfig", customConfig);
+
+  //navbar configuration
+  root.getElementsByClassName("navbar__logo")[0].style.margin =
+    customConfig.navbar["title-margin"];
+  root.style.setProperty(
+    "--ifm-navbar-background-color",
+    customConfig.navbar["background-color"]
+  );
+  root.style.setProperty("--ifm-navbar-height", customConfig.navbar["height"]);
+
+  //header configuration
+
+  //feature configuration
+  const { feature } = customConfig;
+
+  //근데 세팅도큐에서는 이렇게 할게 아니라 스테이트를 읽어와야겠네.
+  //초기값만 json에서 읽어와서 initialState에 넣어줘야될듯.
+  root.getElementsByClassName("linkSection")[0].style.backgroundColor =
+    feature["linkBackground-color"];
+  root.getElementsByClassName(
+    "linkSection"
+  )[0].style.backgroundImage = `url(${feature["linkBackground-image"]})`;
+  root.getElementsByClassName("linkSection")[0].style.height =
+    feature["linkHeight"];
+  root.getElementsByClassName("basicSection")[0].style.backgroundColor =
+    feature["basicBackground-color"];
+  root.getElementsByClassName(
+    "basicSection"
+  )[0].style.backgroundImage = `url(${feature["basicBackground-image"]})`;
+  root.getElementsByClassName("basicSection")[0].style.height =
+    feature["basicHeight"];
+};
