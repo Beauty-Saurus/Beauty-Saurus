7e912be571f8d37fe7284e27c0d841e28ea5ab8d
kunlee <leekh716@naver.com>
2021-11-28 21:28:47 +0900

    :art: fix:bottom beatysaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Beauty-Saurus, Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
